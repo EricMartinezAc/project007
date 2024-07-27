8054f32244df1e69182326c74e5ed55c0cb08f65
Eric Martinez Acelas <proyectos.sieng@gmail.com>
2024-07-27 12:57:07 -0500

    render

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+import { UserConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  envPrefix: "VITE_",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+} as UserConfig);
